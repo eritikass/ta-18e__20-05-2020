ed433c9635f5e056e54b4be5bbde2bc90aebeb8b
SimkaBoi <simkaviiner@gmail.com>
2020-05-20 19:04:32 +0300

    add tests, edit lint rules and again edit the getsum function

--- getSum.js
+++ getSum.js
@@ -1,9 +1,12 @@
 const fetch = require("node-fetch");
 
 async function GetSum(x, y){
-	let response = await fetch(`https://upload.kassikas.com/tpt-sum/?number1=${x}&number2=${y}`).catch(err => console.error(err));
-	let data = await response.json();
-	return data.sum;
+	let data;
+	await fetch(`https://upload.kassikas.com/tpt-sum/?number1=${x}&number2=${y}`)
+		.then(response => response.json())
+		.then(json => data = json.sum)
+		.catch(err => console.error(err));
+	return data;
 }
 
 exports.GetSum = GetSum;

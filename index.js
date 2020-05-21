506ed973e64c782f8c17f1520a0d0de32d64b1b0
AnderoV <andero.voosalu@tptlive.ee>
2020-05-22 00:35:06 +0300

    appi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const sum = require("./sum")
-test()
+const sum = require('./sum');
+test();
 async function test() {
     const a = await sum.kum(1, 2);
-    console.log(a)
+    console.log(a);
 }
--- sum.js
+++ sum.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 let kum = async (number1, number2) => {
     let response = await fetch(`https://upload.kassikas.com/tpt-sum/?number1=${number1}&number2=${number2}`).catch(error => console.error(error));

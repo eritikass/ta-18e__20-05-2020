77cf9679ed7da6bcf33ae69a8c56aef7a1caf6a5
AnderoV <andero.voosalu@tptlive.ee>
2020-05-22 00:53:58 +0300

    tule taevas appi

--- index.js
+++
@@ -1,6 +0,0 @@
-const sum = require('./sum');
-test();
-async function test() {
-    const total = await sum.sum(1, 2);
-    console.log(total);
-}
--- sum.test.js
+++ sum.test.js
@@ -1,12 +1,12 @@
-jest.mock("./sum.js")
-const sum = require ("./sum.js")
+jest.mock('./sum.js');
+const sum = require ('./sum.js');
 
 test('3', async () => {
     const total = await sum.sum(1, 2);
-    expect(total).toBe(3)
+    expect(total).toBe(3);
 });
 
 test('5', async () => {
     const total = await sum.sum(3, 2);
-    expect(total).toBe(5)
+    expect(total).toBe(5);
 });
